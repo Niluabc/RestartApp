eef723c87f6e95f98f11f2d3769016ff16fc3068
Nilam <shahnilam812@gmail.com>
2023-11-07 09:27:44 +0530

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Nilam Shah on 07/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
